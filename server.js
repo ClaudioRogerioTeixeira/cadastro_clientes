62b7717d51c121551fc5a35484bd80fdc38da3d5
Teixeira <claudioteixeira@outlook.com>
2022-04-11 10:31:19 -0300

    Alteração deploy Heroku

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// importar express
 const express = require('express');
-// iniciar express
 const app = express();
-// nome da pasta no dist que será feito o build
-const appName = 'cadastro-clientes';
-// local onde build irá gerar os arquivos
-const outputPath = `${__dirname}/dist/${appName}`
 
-// seta o diretório de build para servir o conteúdo Angular
-app.use(express.static(outputPath));
-// redirecionar qualquer requisição para o index.html
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/cadastro-clientes'));
+
 app.get('/*', (req, res) => {
-  res.sendFile(`$(outputPath)/index.html`);
+  res.sendFile(__dirname + '/dist/cadastro-clientes/index.html');
 });
-// ouvir a porta que o Heroku disponibilizar
-app.listen(process.enf.PORT);
+
+app.listen(PORT, () => {
+  console.log('Servidor iniciado na porta ' + PORT);
+});
