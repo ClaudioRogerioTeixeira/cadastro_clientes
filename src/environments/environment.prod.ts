e25cdcd6c5e595a72873d87ef31fd69fe8f70937
Teixeira <claudioteixeira@outlook.com>
2022-04-27 11:16:35 -0300

    Alteração dialog exclusão

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: '/api'
-  // baseUrl: 'https://app-clientes-api.herokuapp.com'
+  // baseUrl: '/api'
+  baseUrl: 'https://app-clientes-api.herokuapp.com'
 };
